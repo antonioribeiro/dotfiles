2fc215e6f372a22a78a49b096e5a8706785bf29b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2020-01-05 19:26:15 +0100

    Remove config from repository

--- config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export ___HOSTNAME___=acr-macbookpro
-export ___USERNAME___=antoniocarlosribeirofaria
-export ___USERGROUP___=staff
-export ___NAME___="Antonio Ribeiro"
-export ___EMAIL___="acr@antoniocarlosribeiro.com"
