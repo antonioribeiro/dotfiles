db451f4b279917ab8300eedf30271e258d3887f0
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2022-07-23 15:02:42 +0200

    Remove some paths

--- paths.sh
+++ paths.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+PATH="${PATH}:/System/Volumes/Data/opt/homebrew/bin"
 PATH="${PATH}:$HOME/perl5/bin"
 PATH="${PATH}:$HOME/.nvm"
 PATH="${PATH}:$HOME/bin"
@@ -10,8 +11,5 @@ PATH="${PATH}:$HOME/.dotfiles/bin"
 PATH="${PATH}:/Users/$DEFAULT_USER/.rvm/gems/ruby-2.1.2/bin"
 PATH="${PATH}:/usr/local/bin"
 PATH="${PATH}:$HOME/.yarn/bin"
-PATH="${PATH}:/usr/local/opt/mysql@5.7/bin"
-PATH="${PATH}:/usr/local/opt/node@8/bin"
-PATH="${PATH}:/System/Volumes/Data/opt/homebrew/bin"
 
 export PATH
