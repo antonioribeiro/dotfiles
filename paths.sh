5f4134c807bcca024336621dc8526d019850aa66
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2022-07-23 15:03:14 +0200

    Update paths

--- paths.sh
+++ paths.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
+PATH="${PATH}:/usr/local/bin"
+PATH="${PATH}:$HOME/bin"
 PATH="${PATH}:/System/Volumes/Data/opt/homebrew/bin"
 PATH="${PATH}:$HOME/perl5/bin"
 PATH="${PATH}:$HOME/.nvm"
-PATH="${PATH}:$HOME/bin"
 PATH="${PATH}:$HOME/.rvm/bin"
 PATH="${PATH}:$_COMPOSER_BIN"
 PATH="${PATH}:$NPM_PACKAGES/bin"
 PATH="${PATH}:$HOME/.dotfiles/bin"
 PATH="${PATH}:/Users/$DEFAULT_USER/.rvm/gems/ruby-2.1.2/bin"
-PATH="${PATH}:/usr/local/bin"
 PATH="${PATH}:$HOME/.yarn/bin"
 
 export PATH
